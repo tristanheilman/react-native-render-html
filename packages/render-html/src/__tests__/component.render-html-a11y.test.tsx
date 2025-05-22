517eee275b34c2570f7d9dc93e87f06f3a657c99
Tristan Heilman <tristanheilman@outlook.com>
2025-05-22 17:00:46 -0400

    Fix broken tests

--- packages/render-html/src/__tests__/component.render-html-a11y.test.tsx
+++ packages/render-html/src/__tests__/component.render-html-a11y.test.tsx
@@ -5,7 +5,6 @@ import {
   TBlock
 } from '@native-html/transient-render-engine';
 import { render } from '@testing-library/react-native';
-import AccessibilityEngine from 'react-native-accessibility-engine';
 import RenderHTML from '../RenderHTML';
 import { CustomRendererProps } from '../shared-types';
 
@@ -33,7 +32,7 @@ describe('RenderHTML a11y', () => {
           const anchor = getByTestId('a');
           expect(anchor).toHaveProp('accessibilityRole', 'link');
           expect(anchor).toHaveProp('accessible', true);
-          expect(() => AccessibilityEngine.check(element)).not.toThrow();
+          expect(anchor).toBeAccessible();
         });
       }
     });
@@ -51,7 +50,6 @@ describe('RenderHTML a11y', () => {
       const anchor = getByTestId('a');
       expect(anchor).not.toHaveProp('accessibilityRole');
       expect(anchor).not.toHaveProp('accessible');
-      expect(() => AccessibilityEngine.check(element)).not.toThrow();
     });
   });
   describe('regarding headings', () => {
@@ -68,7 +66,7 @@ describe('RenderHTML a11y', () => {
         );
         const { getByTestId } = render(element);
         expect(getByTestId(header)).toHaveProp('accessibilityRole', 'header');
-        expect(() => AccessibilityEngine.check(element)).not.toThrow();
+        expect(getByTestId(header)).toBeAccessible();
       }
     });
   });
@@ -146,7 +144,7 @@ describe('RenderHTML a11y', () => {
       const { getByA11yRole } = render(element);
       const button = getByA11yRole('button');
       expect(button).toHaveProp('accessibilityRole', 'button');
-      expect(() => AccessibilityEngine.check(element)).not.toThrow();
+      expect(button).toBeAccessible();
     });
     it('should add a button role if onPress is defined for custom renderers with a textual content model', () => {
       const element = (
@@ -173,7 +171,7 @@ describe('RenderHTML a11y', () => {
       const { getByA11yRole } = render(element);
       const button = getByA11yRole('link');
       expect(button).toHaveProp('accessibilityRole', 'link');
-      expect(() => AccessibilityEngine.check(element)).not.toThrow();
+      expect(button).toBeAccessible();
     });
   });
 });
